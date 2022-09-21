b0090c652bc4b2f7efba1d4dbc69e96b3e7ab9a4
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2022-09-21 23:07:59 +0600

    With local api

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -2,9 +2,7 @@ import { Project } from "../typings";
 
 export const fetchProjects = async () => {
   const res = await fetch(
-    `${
-      process.env.NEXT_PUBLIC_BASE_URL || "https://portfolio-yt-2-0.vercel.app/"
-    }/api/getProjects`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
   );
 
   const data = await res.json();
